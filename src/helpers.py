a7a4117cb1cecf99212e95de4c1310a8e70b218b
pac256c <peter8pie@gmail.com>
2026-05-08 19:40:10 -0400

    added split_nodes_delimiter function

--- src/helpers.py
+++ src/helpers.py
@@ -14,3 +14,18 @@ def text_node_to_html_node(text_node):
         case TextType.IMAGE: return LeafNode("img", None, props={"src":text_node.url, "alt":text_node.text})
         case _: raise Exception("Error in text_node_to_html_node - type not found")
         
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            txt_arr = node.text.split(delimiter)
+            if len(txt_arr) % 2 != 1:
+                raise Exception(f"Error: unmatched delimiter {delimiter} in text '{node.text}'")
+            for i in range(len(txt_arr)):
+                if txt_arr[i] != "":
+                    if i % 2 == 0: new_nodes.append(TextNode(txt_arr[i],TextType.TEXT))
+                    else:          new_nodes.append(TextNode(txt_arr[i],text_type))
+    return new_nodes
--- src/test/test_helpers.py
+++ src/test/test_helpers.py
@@ -11,6 +11,9 @@ from parentnode import *
 from htmlnode import *
 
 class TestHelpers(unittest.TestCase):
+    ###############################################################################################
+    # text_node_to_html_node
+    ###############################################################################################
     def test_text(self):
         node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
@@ -48,6 +51,66 @@ class TestHelpers(unittest.TestCase):
         self.assertEqual(html_node.tag, "img")
         self.assertEqual(html_node.props["src"], "https://www.google.com")
         self.assertEqual(html_node.props["alt"], "This is an image node")
+
+    ###############################################################################################
+    # split_nodes_delimiter
+    ###############################################################################################
+    def test_codebasic(self):
+        inparr = [
+            TextNode("This is text with a `code block` word", TextType.TEXT)
+        ]
+        exparr = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        resarr = split_nodes_delimiter(inparr, "`", TextType.CODE)
+        self.assertEqual(resarr, exparr)
+
+    def test_boldbasic(self):
+        inparr = [
+            TextNode("This is text with a **bold** word", TextType.TEXT)
+        ]
+        exparr = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ]
+        resarr = split_nodes_delimiter(inparr, "**", TextType.BOLD)
+        self.assertEqual(resarr, exparr)
+
+    def test_italicbasic(self):
+        inparr = [
+            TextNode("This is text with a _italic_ word", TextType.TEXT)
+        ]
+        exparr = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word", TextType.TEXT),
+        ]
+        resarr = split_nodes_delimiter(inparr, "_", TextType.ITALIC)
+        self.assertEqual(resarr, exparr)
+
+    def test_boldmulti(self):
+        inparr = [
+            TextNode("This is text with **multiple** **bold** words", TextType.TEXT)
+        ]
+        exparr = [
+            TextNode("This is text with ", TextType.TEXT),
+            TextNode("multiple", TextType.BOLD),
+            TextNode(" ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" words", TextType.TEXT),
+        ]
+        resarr = split_nodes_delimiter(inparr, "**", TextType.BOLD)
+        self.assertEqual(resarr, exparr)
+
+    def test_boldnone(self):
+        inparr = [
+            TextNode("This is text with no bold words", TextType.TEXT)
+        ]
+        resarr = split_nodes_delimiter(inparr, "**", TextType.BOLD)
+        self.assertEqual(resarr, inparr)
 
 if __name__ == "__main__":
     unittest.main()
