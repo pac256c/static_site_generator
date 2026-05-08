e8c9c2dc38af7dc6ac582cf2808fb3cb04d43ab5
pac256c <peter8pie@gmail.com>
2026-05-07 20:55:18 -0400

    created structure & textnode

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+def main():
+    print("hello world")
+    node = TextNode("hello text", TextType.BOLD, "google.com")
+    print(node)
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode():
+    def __init__(self, text="", text_type=None, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        equiv  = self.text == other.text
+        equiv |= self.text == other.text_type
+        equiv |= self.url  == other.url
+        return equiv
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
